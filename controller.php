7b2cd559c083f006819f79edf33e4d11e3226fb7
Paulo Nakaima <nakaima.prk@gmail.com>
2018-08-15 10:02:58 -0300

    Controller e Index

--- controller.php
+++ controller.php
@@ -6,19 +6,20 @@ require_once("class/Trip.php");
 require_once("class/TripDao.php");
 
 $conexao = pg_connect("host=localhost port=5432 dbname=consystem user=postgres password=postgres")
-or die("Could not connect to server\n");
+or die("Sem conexão com bd\n");
 
 $fh = fopen($_FILES['file']['tmp_name'], 'r+');
 $lines = array();
+
+if($fh == NULL) {
+    header("Location: http://localhost/erro.html");
+}
 while( ($row = fgetcsv($fh, 1000, ";")) !== FALSE ) {
     $lines[] = $row;
     $num = (int) $row[0];
     $duration = (float) $row[1];
     $trip = new Trip($num, $duration);
-    var_dump($num);
-    var_dump($duration);
-    var_dump($trip);
     $tripDao = new TripDao($conexao);
     $tripDao->save($trip);
 }
-
+header("Location: http://localhost/confirmacao.html");
